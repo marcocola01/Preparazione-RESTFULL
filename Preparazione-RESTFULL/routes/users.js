c4aa7ae33df73d262df4dbad4d67e7a3ffe82568
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-03-02 09:34:06 +0000

    continuazione

--- Preparazione-RESTFULL/routes/users.js
+++ Preparazione-RESTFULL/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql');
+const config = {
+    user: '4DD_09',  //Vostro user name
+    password: 'xxx123##', //Vostra password
+    server: "213.140.22.237",  //Stringa di connessione
+    database: '4DD_09', //(Nome del DB)
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/attori', function (req, res, next) {
+    sql.connect(config, err => {
+        if (err) console.log(err);  // ... error check
 
-module.exports = router;
+        // Query
+        let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+        sqlRequest.query("select * from dbo.[cr-unit-attributes]", (err, result) => {
+            if (err) console.log(err); // ... error checks
+            res.send(result);  //Invio il risultato
+        });
+    });
+});
